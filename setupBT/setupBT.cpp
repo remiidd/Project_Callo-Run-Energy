03ead787e40a1276bb9379d7e8f1aa3a2ed8ec8e
Debray Rémi <remi281099@orange.fr>
2016-09-14 17:04:39 +0200

    Add files via upload

---
+++ setupBT/setupBT.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>       
+#include <SoftwareSerial.h>
+
+SoftwareSerial hc06(9,10);
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("ENTER AT Commands:");
+  hc06.begin(9600);              
+}
+
+void loop(){
+  if (hc06.available()){
+    Serial.write(hc06.read());
+  }
+  if (Serial.available()){
+    hc06.write(Serial.read());
+  }  
+}
+
+              
